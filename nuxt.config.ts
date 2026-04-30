d6185118a0834c8762e3d63b0227bec3e876e29c
Daniel Finch <44116828+dlukefinch@users.noreply.github.com>
2026-04-30 18:08:07 +0100

    asdasd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,3 +2,25 @@ export default defineNuxtConfig({
   extends: ['docus'],
   modules: ['nuxt-studio'],
 })
+
+export default defineNuxtConfig({
+studio: {
+  repository: {
+    provider: 'github', // 'github' or 'gitlab'
+    owner: 'dlukefinch',
+    repo: 'MeteoMapsDocs',
+    branch: 'main'
+  }
+}
+})
+
+export default defineNuxtConfig({
+nitro: {
+  prerender: {
+    // Pre-render the homepage
+    routes: ['/'],
+    // Then crawl all the links on the page
+    crawlLinks: true
+  }
+}
+})
