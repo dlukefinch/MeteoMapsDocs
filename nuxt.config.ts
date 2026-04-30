f6fd0e2521560ccf47d4cc81f4292790da8f8944
Daniel Finch <44116828+dlukefinch@users.noreply.github.com>
2026-04-30 18:36:00 +0100

    321

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,3 +2,12 @@ export default defineNuxtConfig({
   extends: ['docus'],
   modules: ['nuxt-studio'],
 })
+
+export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    }
+  }
+})
